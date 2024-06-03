7026d454346f30bf3235eaac5acd97bce6fb5521
oriel <orielarusssi@gmail.com>
2024-06-03 09:14:25 +0300

    first commit

---
+++ react/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="navbar">
+      <div className="title">תחזית מסביב לעולם</div>
+    </div>
+  );
+};
+
+export default Navbar;
